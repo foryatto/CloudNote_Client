57ee1586fbdcf7083279f9402d1418de7cf2a2ff
foryatto <49111878+foryatto@users.noreply.github.com>
2022-02-07 22:19:58 +0800

    update

--- web/src/components/manager/Profile.vue
+++ web/src/components/manager/Profile.vue
@@ -14,30 +14,9 @@
             <el-descriptions-item>
                 <template slot="label">
                     <i class="el-icon-mobile-phone"></i>
-                    手机号
+                    邮箱
                 </template>
-                18100000000
-            </el-descriptions-item>
-            <el-descriptions-item>
-                <template slot="label">
-                    <i class="el-icon-location-outline"></i>
-                    居住地
-                </template>
-                苏州市
-            </el-descriptions-item>
-            <el-descriptions-item>
-                <template slot="label">
-                    <i class="el-icon-tickets"></i>
-                    备注
-                </template>
-                <el-tag size="small">学校</el-tag>
-            </el-descriptions-item>
-            <el-descriptions-item>
-                <template slot="label">
-                    <i class="el-icon-office-building"></i>
-                    联系地址
-                </template>
-                江苏省苏州市吴中区吴中大道 1188 号
+                admin@admin.com
             </el-descriptions-item>
         </el-descriptions>
     </div>
